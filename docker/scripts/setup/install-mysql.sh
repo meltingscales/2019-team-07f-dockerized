80a9e826bfe0747d6dc5974c2be92cc413b226dd
Henry Post <henryfbp@gmail.com>
2020-07-07 16:48:19 -0500

    store password in dockerfile...not ideal. TODO fix

--- docker/scripts/setup/install-mysql.sh
+++ docker/scripts/setup/install-mysql.sh
@@ -2,14 +2,14 @@
 
 echo "Installing MySQL."
 
-if [[ -z $MYSQL_PASSWORD ]]; then
+if [[ -z ${MYSQL_PASSWORD} ]]; then
   echo "MYSQL_PASSWORD must not be empty. Did you forget to pass arguments/environment variables?"
   echo ${MYSQL_PASSWORD}
   exit 1
 fi
 
 # This prevents any prompts from coming up.
-echo "mysql-server-5.7 mysql-server/root_password $MYSQL_PASSWORD $MYSQL_PASSWORD"        | debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again $MYSQL_PASSWORD $MYSQL_PASSWORD"  | debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password password ${MYSQL_PASSWORD}"        | debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password ${MYSQL_PASSWORD}"  | debconf-set-selections
 
 apt-get install -y mysql-server-5.7 mysql-client
