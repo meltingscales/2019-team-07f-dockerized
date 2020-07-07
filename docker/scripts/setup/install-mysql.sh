420b8f54ad2fe08790c16b4d20b3897c82e2f8d6
Henry Post <henryfbp@gmail.com>
2020-07-07 16:07:57 -0500

    no sudo on docker, also, uh...passowrd security pls?

--- docker/scripts/setup/install-mysql.sh
+++ docker/scripts/setup/install-mysql.sh
@@ -4,11 +4,12 @@ echo "Installing MySQL."
 
 # Password is 'password'.
 # This also prevents any prompts from coming up.
-echo "mysql-server-5.7 mysql-server/root_password password password"        | sudo debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again password password"  | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password password password"        | debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password password"  | debconf-set-selections
+
+apt-get install -y mysql-server-5.7 mysql-client
 
 echo "Hey Henry -- You must secure this before you use it. 'password' sucks as a password."
 echo "Security by default, right? ;)"
+echo "IDK, past self... bruh... Use uh...Vault? K8s? Not sure. Google!!!"
 exit 1
-
-apt-get install -y mysql-server-5.7 mysql-client
