fcb1bd4ea430c82a144f8bf24a3532de6140d485
Henry Post <henryfbp@gmail.com>
2020-07-07 16:12:52 -0500

    force user to provide password via environment variables

--- docker/scripts/setup/install-mysql.sh
+++ docker/scripts/setup/install-mysql.sh
@@ -2,14 +2,13 @@
 
 echo "Installing MySQL."
 
-# Password is 'password'.
-# This also prevents any prompts from coming up.
-echo "mysql-server-5.7 mysql-server/root_password password password"        | debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again password password"  | debconf-set-selections
+if [[ -z $MYSQL_PASSWORD ]]; then
+  echo "MYSQL_PASSWORD must not be empty. Did you forget to pass arguments/environment variables?"
+  exit 1
+fi
+
+# This prevents any prompts from coming up.
+echo "mysql-server-5.7 mysql-server/root_password $MYSQL_PASSWORD $MYSQL_PASSWORD"        | debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again $MYSQL_PASSWORD $MYSQL_PASSWORD"  | debconf-set-selections
 
 apt-get install -y mysql-server-5.7 mysql-client
-
-echo "Hey Henry -- You must secure this before you use it. 'password' sucks as a password."
-echo "Security by default, right? ;)"
-echo "IDK, past self... bruh... Use uh...Vault? K8s? Not sure. Google!!!"
-exit 1
